d0f439fa9945281228fc43ddfdc940f74e51e10b
nariman <bluesman.in@gmail.com>
2018-09-24 14:49:57 +0300

    notes_app added

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,20 @@
 // function register() {
 	
 // }
-
+var backdrop = document.getElementById('backdrop');
+var newNote = document.getElementById('newNote');
 function addNote() {
-	var backdrop = document.getElementById('backdrop');
-	var newNote = document.getElementById('newNote');
 	backdrop.style.display = 'block';
 	backdrop.style.zIndex = '10';
 	newNote.style.display = 'block';
 	newNote.style.zIndex = '20';
 }
-
 function cancel_note() {
 	backdrop.style.display = 'none';
 	newNote.style.display = 'none';
 }
 
-// Получение всех заметок
+// // Получение всех заметок
 // function GetNotes() {
 // 	$.ajax({
 // 		url: "/notes",
@@ -27,7 +25,7 @@ function cancel_note() {
 // 			$.each(notes, function (index, note) {
 // 				// добавляем полученные элементы в таблицу
 // 				rows += row(note);
-// 			})
+// 			});
 // 			$("table tbody").append(rows);
 // 		}
 // 	});
@@ -47,34 +45,41 @@ function cancel_note() {
 // 		}
 // 	});
 // }
-
+$(document).ready(function() {
 	// отправка формы
-$("#notesForm").submit(function(event) {
-	event.preventDefault();
-	console.log(event.target);
-	var note = $(event.target).serialize();
-	console.log(note);
+	$("#notesForm").submit(function(event) {
+		event.preventDefault();
+		console.log(event.target);
+		var note = $(event.target).serialize();
 		CreateNote(note);
-	// if (id == 0)
-	// 	CreateNote(note);
-	// else
-	// 	EditNote(note);
+		// if (id == 0)
+		// 	CreateNote(note);
+		// else
+		// 	EditNote(note);
+	});
+
+		// Добавление заметки
+	function CreateNote(data) {
+		$.ajax({
+			url: "/notes",
+			type: 'POST',
+			data: data,
+			success: function(note) {
+				$("table tbody").append(row(note));
+				backdrop.style.display = 'none';
+				newNote.style.display = 'none';
+				
+			}
+		});
+	};
 });
 
-	// Добавление заметки
-function CreateNote() {
-	$.ajax({
-		url: "/notes",
-		type: 'POST',
-		data: note,
-		success: function(note) {
-			$("table tbody").append(row(note));
-		}
-	});
+// создание строки для таблицы
+var n = 0;
+function row(note) {
+	n++;
+	return "<tr id=" + n + " data-rowid=" + note._id + "'><td>" + note.created + "</td><td>" + note.created + "</td>" +
+		   "<td>" + note.note_name + "</td> <td>" + note.note_theme + "</td></tr>";
 };
 
-// создание строки для таблицы
-function row(note) {
-	return "<tr data-rowid='" + note._id + "'><td>" + note.created + "</td><td>" + note.created + "</td>" +
-		   "<td>" + note.noteName + "</td> <td>" + note.noteTheme + "</td></tr>";
-}
+// GetNotes();
