283eaa6164c199d14f7623c7d0d95ca40cfcbe10
nariman <bluesman.in@gmail.com>
2018-09-25 14:54:29 +0300

    intermediate version

--- public/js/main.js
+++ public/js/main.js
@@ -1,62 +1,40 @@
 // function register() {
 	
 // }
-var backdrop = document.getElementById('backdrop');
-var newNote = document.getElementById('newNote');
-function addNote() {
-	backdrop.style.display = 'block';
-	backdrop.style.zIndex = '10';
-	newNote.style.display = 'block';
-	newNote.style.zIndex = '20';
-}
-function cancel_note() {
-	backdrop.style.display = 'none';
-	newNote.style.display = 'none';
-}
-
-// // Получение всех заметок
-// function GetNotes() {
-// 	$.ajax({
-// 		url: "/notes",
-// 		type: "GET",
-// 		contentType: "application/json",
-// 		success: function (notes) {
-// 			var rows = "";
-// 			$.each(notes, function (index, note) {
-// 				// добавляем полученные элементы в таблицу
-// 				rows += row(note);
-// 			});
-// 			$("table tbody").append(rows);
-// 		}
-// 	});
-// }
-
-//Получение одной заметки
-// function GetNote(id) {
-// 	$.ajax({
-// 		url: "/notes/"+id,
-// 		type: "GET",
-// 		contentType: "application/json",
-// 		success: function (note) {
-// 			var form = document.forms["noteForm"];
-// 			form.elements["note_name"].value = note.note_name;
-// 			form.elements["note_theme"].value = note.note_theme;
-// 			form.elements["content"].value = note.content;
-// 		}
-// 	});
-// }
 $(document).ready(function() {
+
+	var backdrop = document.getElementById('backdrop');
+	var newNote = document.getElementById('newNote');
+
+	$('#add_note').click(function() {
+		backdrop.style.display = 'block';
+		backdrop.style.zIndex = '10';
+		newNote.style.display = 'block';
+		newNote.style.zIndex = '20';
+	});
+
+	$('#cancel_note').click(function() {
+		backdrop.style.display = 'none';
+		newNote.style.display = 'none';
+	});
+
+
+	// Получение всех заметок
+	// function GetNotes() {
+
+
+	//Получение одной заметки
+	// function GetNote(id) {
+
+
 	// отправка формы
 	$("#notesForm").submit(function(event) {
 		event.preventDefault();
 		console.log(event.target);
 		var note = $(event.target).serialize();
 		CreateNote(note);
-		// if (id == 0)
-		// 	CreateNote(note);
-		// else
-		// 	EditNote(note);
 	});
+
 
 		// Добавление заметки
 	function CreateNote(data) {
@@ -72,14 +50,14 @@ $(document).ready(function() {
 			}
 		});
 	};
+
+	// создание строки для таблицы
+	var n = 0;
+	function row(note) {
+		n++;
+		return "<tr id=" + n + " data-rowid=" + note._id + "'><td>" + note.created + "</td><td>" + note.created + "</td>" +
+			   "<td>" + note.note_name + "</td> <td>" + note.note_theme + "</td></tr>";
+	};
+
 });
 
-// создание строки для таблицы
-var n = 0;
-function row(note) {
-	n++;
-	return "<tr id=" + n + " data-rowid=" + note._id + "'><td>" + note.created + "</td><td>" + note.created + "</td>" +
-		   "<td>" + note.note_name + "</td> <td>" + note.note_theme + "</td></tr>";
-};
-
-// GetNotes();
--- routes/index_templates.js
+++ routes/index_templates.js
@@ -1,4 +1,5 @@
 var mongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/usersdb";
 
 module.exports = function(app) {
 	app.get('/signup', require('./signup').get);
@@ -10,27 +11,4 @@ module.exports = function(app) {
 	app.get('/notes', require('./notes').get);
 	app.post('/notes', require('./notes').post);
 
-
-	// поиск всех заметок в БД
-	// app.get("/notes", function(request, response){
-
-	// 	mongoClient.connect(url, {useNewUrlParser: true}, function(err, client){
-	// 		client.db("usersdb").collection("notes").find({/*author: request.session.user*/}).toArray(function(err, notes){
-	// 			response.send(notes);
-
-	// 			client.close();
-	// 		});
-	// 	});
-	// });
-	//поиск одной заметки
-	// app.get("/notes/:id", function(request, response){
-	// 	var id = new objectId(request.params.id);
-	// 	mongoClient.connect(url, {useNewUrlParser: true}, function(err, client){
-	// 		client.db("usersdb").collection("notes").findOne({_id: id}, function(err, note){
-	// 			if(err) return response.status(400).send();
-	// 			response.send(note);
-	// 			client.close();
-	// 		});
-	// 	});
-	// });
 };
